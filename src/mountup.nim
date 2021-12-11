8f58b87c4e59b6d9b204f295949108cb4f70fa70
Mutsuha Asada <momiimt@gmail.com>
2021-12-11 22:53:44 +0900

    👍 Add: Implemented on a momentum

--- src/mountup.nim
+++ src/mountup.nim
@@ -2,4 +2,30 @@ when isMainModule:
   discard
 
 proc mtupParse* (rawMtup: string): string =
-  discard
+  result = "<p>"
+  var commandFlag = false
+  var boldFlag = false
+  var firstWhiteSpace = false
+  for rawMtupChar in rawMtup:
+    if rawMtupChar == '[':
+      commandFlag = true
+    elif commandFlag:
+      if rawMtupChar == '*':
+        boldFlag = true
+        result.add "<b>"
+        firstWhiteSpace = true
+      elif rawMtupChar == ']':
+        commandFlag = false
+        if boldFlag:
+          result.add "</b>"
+          boldFlag = false
+      elif rawMtupChar == ' ':
+        if firstWhiteSpace:
+          firstWhiteSpace = false
+        else:
+          result.add rawMtupChar
+      else:
+        result.add rawMtupChar
+    else:
+      result.add rawMtupChar
+  result.add "</p>"

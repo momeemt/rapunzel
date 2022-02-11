966c2e6df215b1466e5664f60848bec13bfd7f4e
Mutsuha Asada <momiimt@gmail.com>
2022-02-11 15:57:20 +0900

    🧪 implement header command test

---
+++ tests/test_header.nim
@@ -0,0 +1,22 @@
+import unittest
+import rapunzel
+
+proc getHtmlContentsFromAssetsHeaders (name: string): string =
+  block:
+    let
+      path = "tests/assets/headers/" & name & ".html"
+      file = open(path, fmRead)
+    defer:
+      file.close()
+    result = file.readAll()
+
+test "Header command":
+  const rapunzel = """
+{* Header1}
+{** Header2}
+{*** Header3}
+{**** Header4}
+{***** Header5}
+{****** Header6}
+"""
+  check rapunzel.rapunzelParse.astToHtml() == getHtmlContentsFromAssetsHeaders("header")

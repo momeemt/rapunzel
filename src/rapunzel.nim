234ee9c68ead15e6f56f95bfc72a1167da811524
Mutsuha Asada <momiimt@gmail.com>
2022-02-12 18:18:41 +0900

    ♻️ cutting utils from rapunzel

--- src/rapunzel.nim
+++ src/rapunzel.nim
@@ -1,5 +1,2 @@
-proc isHexadecimal (maybeHex: string): bool =
-  result = true
-  for character in maybeHex:
-    if character notin {'0'..'9', 'a'..'f', 'A'..'F'}:
-      return false
+import rapunzel/[types, parser, node, generator, formatHTML, utils]
+export types, parser, node, generator, formatHtml, utils
---
+++ src/rapunzel/utils.nim
@@ -0,0 +1,5 @@
+proc isHexadecimal* (maybeHex: string): bool =
+  result = true
+  for character in maybeHex:
+    if character notin {'0'..'9', 'a'..'f', 'A'..'F'}:
+      return false

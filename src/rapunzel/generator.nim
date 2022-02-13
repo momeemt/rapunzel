e08b1690ff7c973568c15748c2fcdf00690d1504
Mutsuha Asada <momiimt@gmail.com>
2022-02-13 17:45:04 +0900

    ♻️ change childrenValue to childrenAstToHtml

--- src/rapunzel/generator.nim
+++ src/rapunzel/generator.nim
@@ -4,30 +4,30 @@ import node, types
 var mtupVarsTable = initTable[string, string]()
 mtupVarsTable["now"] = ""
 
-proc childrenValue (ast: RapunzelNode): string
+proc childrenAstToHtml (ast: RapunzelNode): string
 
 proc astToHtml* (ast: RapunzelNode): string =
   result = case ast.kind:
   of rapunzelText: ast.value.strip(true, false, {'\t'})
   of rapunzelBold:
     if ast.children.len == 0: "<b>" & ast.value.strip(true, false, {'\t'}) & "</b>"
-    else: "<b>" & ast.childrenValue & "</b>"
+    else: "<b>" & ast.childrenAstToHtml & "</b>"
   of rapunzelItalic:
     if ast.children.len == 0: "<em>" & ast.value.strip(true, false, {'\t'}) & "</em>"
-    else: "<em>" & ast.childrenValue & "</em>"
+    else: "<em>" & ast.childrenAstToHtml & "</em>"
   of rapunzelStrike:
     if ast.children.len == 0: "<span class=\"rapunzel--strike\">" & ast.value.strip(true, false, {'\t'}) & "</span>"
-    else: "<span class=\"rapunzel--strike\">" & ast.childrenValue & "</span>"
+    else: "<span class=\"rapunzel--strike\">" & ast.childrenAstToHtml & "</span>"
   of rapunzelUnderline:
     if ast.children.len == 0: "<span class=\"rapunzel--underline\">" & ast.value.strip(true, false, {'\t'}) & "</span>"
-    else: "<span class=\"rapunzel--underline\">" & ast.childrenValue & "</span>"
+    else: "<span class=\"rapunzel--underline\">" & ast.childrenAstToHtml & "</span>"
   of rapunzelColor:
     if ast.children.len == 0: "<span style=\"color: " & ast.colorCode & ";\">" & ast.value.strip(true, false, {'\t'}) & "</span>"
-    else: "<span style=\"color: " & ast.colorCode & ";\">" & ast.childrenValue & "</span>"
+    else: "<span style=\"color: " & ast.colorCode & ";\">" & ast.childrenAstToHtml & "</span>"
   of rapunzelHeader:
     let tagName = "h" & $ast.headerRank
     if ast.children.len == 0: &"<{tagName}>" & ast.value.strip(true, false, {'\t'}) & &"</{tagName}>"
-    else: &"<{tagName}>" & ast.childrenValue & &"</{tagName}>"
+    else: &"<{tagName}>" & ast.childrenAstToHtml & &"</{tagName}>"
   of rapunzelVariable:
     let
       varName = ast.value.split(',')[0].strip
@@ -47,10 +47,10 @@ proc astToHtml* (ast: RapunzelNode): string =
     else:
       raise newException(KeyError, &"Variable {ast.value} is undefined.")
     res
-  of rapunzelParagraph: "<p>" & ast.childrenValue & "</p>"
-  of rapunzelDocument, rapunzelBlock: ast.childrenValue
+  of rapunzelParagraph: "<p>" & ast.childrenAstToHtml & "</p>"
+  of rapunzelDocument, rapunzelBlock: ast.childrenAstToHtml
   of rapunzelNone: "" # Todo: 例外を投げる
 
-proc childrenValue (ast: RapunzelNode): string =
+proc childrenAstToHtml (ast: RapunzelNode): string =
   for child in ast.children:
     result &= child.astToHtml()

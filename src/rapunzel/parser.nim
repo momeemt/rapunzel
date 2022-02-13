1c666fcc5780eb9f524f7c191ec29337760db880
Mutsuha Asada <momiimt@gmail.com>
2022-02-13 16:27:32 +0900

    ♻️ refactoring parser

--- src/rapunzel/parser.nim
+++ src/rapunzel/parser.nim
@@ -1,20 +1,35 @@
-import std/[json, strformat, tables]
+import std/[json, strformat, strutils, tables]
 import node, types, utils
+
+type
+  RapunzelStream = object
+    index: int
+    str: string
+    openingParenthesisCount: int # 閉じられていない '[', '{' の個数
+    node: RapunzelNode
 
 let colorJson = parseFile("assets/colorPalette.json").getFields
 var colorJsonKey: seq[string]
 for key in colorJson.keys: colorJsonKey.add key
 
-func updateChildNode (src, child: RapunzelNode, openingParenthesisCount: int): RapunzelNode =
+func initRapunzelStream (str: string): RapunzelStream =
+  result = RapunzelStream(
+    index: 0,
+    str: str,
+    openingParenthesisCount: 0,
+    node: RapunzelNode(kind: rapunzelDocument)
+  )
+
+proc updateChildNode (stream: var RapunzelStream, child: RapunzelNode) =
   if child.kind == rapunzelNone:
-    return src
+    return
   if child.kind == rapunzelText and child.value == "": # 空のテキストノードを追加しない
-    return src
+    return
   var nodeSeqByDepth = @[new RapunzelNode]
-  nodeSeqByDepth[0][] = src
+  nodeSeqByDepth[0][] = stream.node
   # openingParenthesisCountが0の時、テキストノードを扱っている
   # ネスト数は閉じられていない左括弧より、1少なくなる
-  var nestCount = max(openingParenthesisCount-1, 0)
+  var nestCount = max(stream.openingParenthesisCount-1, 0)
   # 更新する部分木とその経路を保存する
   for index in 0..nestCount:
     let
@@ -26,129 +41,138 @@ func updateChildNode (src, child: RapunzelNode, openingParenthesisCount: int): R
   # 更新した部分木で経路を元に親の部分木を更新する
   for index in countdown(nestCount, 0):
     nodeSeqByDepth[index][].children[nodeSeqByDepth[index][].children.high] = nodeSeqByDepth[index+1][]
-  result = nodeSeqByDepth[0][]
+  stream.node = nodeSeqByDepth[0][]
 
-proc rapunzelParse* (rawRapunzel: string): RapunzelNode =
-  result = RapunzelNode(kind: rapunzelDocument)
-  if rawRapunzel.len >= 2 and rawRapunzel[0] == '{':
-    result.children.add RapunzelNode(kind: rapunzelBlock)
+func inlineCommandName (stream: RapunzelStream): string =
+  for ch in stream.str[stream.index+1..^1]:
+    if ch == ' ': return
+    result.add ch
+
+func blockCommandName (stream: RapunzelStream): string =
+  for ch in stream.str[stream.index+1..^1]:
+    if ch == ' ' or ch == '\n': return
+    result.add ch
+
+func isUnderAnalysis (stream: RapunzelStream): bool =
+  result = stream.index < stream.str.len
+
+proc next (stream: var RapunzelStream) =
+  stream.index += 1
+
+proc skip (stream: var RapunzelStream, charCount: int) =
+  stream.index += charCount
+
+func char (stream: RapunzelStream): char =
+  result = stream.str[stream.index]
+
+func char (stream: RapunzelStream, index: int): char =
+  result = stream.str[stream.index + index]
+
+proc genRapunzelColor (commandName: string): RapunzelNode =
+  let
+    color = commandName[1..^1]
+    colorCode = if color in colorJsonKey:
+      colorJson[color].getStr
+    elif color.isHexadecimal: commandName
+    else: raise newException(UndefinedColorDefect, &"{color} is undefined color or incorrect color code")
+  result = RapunzelNode(kind: rapunzelColor, colorCode: colorCode)
+
+proc genRapunzelHeader (commandName: string): RapunzelNode =
+  if commandName != "" and commandName.allCharsInSet({'*'}):
+    if commandName.len <= 6:
+      result = RapunzelNode(kind: rapunzelHeader, headerRank: commandName.len.uint8)
+    else:
+      raise newException(
+        UndefinedHeaderRankDefect,
+        &"Only up to 6 header ranks are supported. {commandName.len} is undefined."
+      )
   else:
-    result.children.add RapunzelNode(kind: rapunzelParagraph)
-  var childNode = RapunzelNode(kind: rapunzelNone)
-  var skipCount = 0
-  var openingParenthesisCount = 0 # 閉じられていない '[', '{' の個数
-  for index in 0..rawRapunzel.high:
-    if skipCount > 0:
-      skipCount -= 1
-      continue
-    let rawRapunzelChar = rawRapunzel[index]
-    if rawRapunzelChar == '[':
-      result = result.updateChildNode(childNode, openingParenthesisCount)
-      childNode = RapunzelNode(kind: rapunzelNone)
-      openingParenthesisCount += 1
+    raise newException(UndefinedCommandDefect, &"{commandName} is undefined command.")
 
-      let nextCharacter = rawRapunzel[index+1]
-      skipCount = 2 # [? ...]の "? "をスキップ
+proc parseLeftBracket (stream: var RapunzelStream): RapunzelNode =
+  stream.openingParenthesisCount += 1
+  let
+    commandName = stream.inlineCommandName
+    nextCharacter = stream.char(+1)
+  result = case nextCharacter:
+    of '*': RapunzelNode(kind: rapunzelBold)
+    of '/': RapunzelNode(kind: rapunzelItalic)
+    of '_': RapunzelNode(kind: rapunzelUnderline)
+    of '~': RapunzelNode(kind: rapunzelStrike)
+    of '=': RapunzelNode(kind: rapunzelExpand)
+    of '#': genRapunzelColor(commandName)
+    else:
+      raise newException(UndefinedCommandDefect, &"{commandName} is undefined command.")
+  stream.skip(commandName.len + 1)
 
-      childNode = case nextCharacter:
-      of '*': RapunzelNode(kind: rapunzelBold)
-      of '/': RapunzelNode(kind: rapunzelItalic)
-      of '_': RapunzelNode(kind: rapunzelUnderline)
-      of '~': RapunzelNode(kind: rapunzelStrike)
-      of '=': RapunzelNode(kind: rapunzelExpand)
-      of '#':
-        var
-          color = ""
-          colorIndex = 2 # color-command-nameを取得するためのindex
-        while rawRapunzel[index+colorIndex] != ' ':
-          color.add rawRapunzel[index+colorIndex]
-          colorIndex += 1
-        let colorCode = if color in colorJsonKey:
-          colorJson[color].getStr
-        elif color.isHexadecimal:
-          "#" & color
-        else:
-          raise newException(UndefinedColorDefect, &"{color} is undefined color or incorrect color code")
-        skipCount += colorIndex - 2
-        RapunzelNode(kind: rapunzelColor, colorCode: colorCode)
-      else:
-        var
-          name = ""
-          nameIndex = 2
-        while rawRapunzel[index+nameIndex] != ' ':
-          name.add rawRapunzel[index+nameIndex]
-          nameIndex += 1
-        raise newException(UndefinedCommandDefect, &"{name} is undefined command.")
-    elif rawRapunzelChar == '{':
-      result = result.updateChildNode(childNode, openingParenthesisCount)
-      childNode = RapunzelNode(kind: rapunzelNone)
-      openingParenthesisCount += 1
+proc parseLeftCurlyBracket (stream: var RapunzelStream): RapunzelNode =
+  stream.openingParenthesisCount += 1
+  let
+    commandName = stream.blockCommandName
+    nextCharacter = stream.char(+1)
+  result = case nextCharacter:
+    of '%': RapunzelNode(kind: rapunzelVariable)
+    of '=': RapunzelNode(kind: rapunzelExpand)
+    of '*': genRapunzelHeader(commandName)
+    else:
+      raise newException(UndefinedCommandDefect, &"{commandName} is undefined command.")
+  stream.skip(commandName.len + 1)
 
-      let nextCharacter = rawRapunzel[index+1]
-      skipCount = 2
+proc parseRightBracket (stream: var RapunzelStream): RapunzelNode =
+  stream.openingParenthesisCount -= 1
+  result = RapunzelNode(kind: rapunzelNone)
 
-      childNode = case nextCharacter:
-      of '%': RapunzelNode(kind: rapunzelVariable)
-      of '=': RapunzelNode(kind: rapunzelExpand)
-      of '*':
-        var
-          headerRank = 1'u8
-          headerIndex = 2 # header-rankを取得するためのindex
-        while rawRapunzel[index+headerIndex] != ' ' and rawRapunzel[index+headerIndex] != '\n':
-          if rawRapunzel[index+headerIndex] == '*':
-            headerRank += 1
-            headerIndex += 1
-          else:
-            var
-              name = ""
-              nameIndex = 1
-            while rawRapunzel[index+nameIndex] != ' ':
-              name.add rawRapunzel[index+nameIndex]
-              nameIndex += 1
-            raise newException(UndefinedCommandDefect, &"{name} is undefined command.")
-        if headerRank > 6:
-          raise newException(UndefinedHeaderRankDefect, &"Only up to 6 header ranks are supported. {headerRank} is undefined.")
-        skipCount += headerIndex - 2
-        RapunzelNode(kind: rapunzelHeader, headerRank: headerRank)
-      else:
-        var
-          name = ""
-          nameIndex = 2
-        while rawRapunzel[index+nameIndex] != ' ':
-          name.add rawRapunzel[index+nameIndex]
-          nameIndex += 1
-        raise newException(UndefinedCommandDefect, &"{name} is undefined command.")
-      
-    elif rawRapunzelChar == ']':
-      result = result.updateChildNode(childNode, openingParenthesisCount)
-      childNode = RapunzelNode(kind: rapunzelNone)
-      openingParenthesisCount -= 1
-    elif rawRapunzelChar == '}':
-      result = result.updateChildNode(childNode, openingParenthesisCount)
-      childNode = RapunzelNode(kind: rapunzelNone)
-      openingParenthesisCount -= 1
-      if rawRapunzel.high >= index + 2:
-        if rawRapunzel[index+2] == '{':
-          result.children.add RapunzelNode(kind: rapunzelBlock)
-        else:
-          result.children.add RapunzelNode(kind: rapunzelParagraph)
-      skipCount = 1 # ここで親ノードを追加しているから \n で重複する
-    elif rawRapunzelChar == '\n':
-      if result.children[result.children.high].kind == rapunzelBlock:
+proc addBlockOrParagraph (stream: var RapunzelStream, index: int) =
+  if stream.char(index) == '{':
+    stream.node.children.add RapunzelNode(kind: rapunzelBlock)
+  else:
+    stream.node.children.add RapunzelNode(kind: rapunzelParagraph)
+
+proc parseRightCurlyBracket (stream: var RapunzelStream): RapunzelNode =
+  stream.openingParenthesisCount -= 1
+  result = RapunzelNode(kind: rapunzelNone)
+  if stream.str.high >= stream.index + 2:
+    stream.addBlockOrParagraph(+2)
+  stream.next() # ノード追加が　\n と重複するから, }直後が `\n`　であるかどうかも調べる
+
+proc rapunzelParse* (str: string): RapunzelNode =
+  var
+    stream = initRapunzelStream(str)
+    childNode = RapunzelNode(kind: rapunzelNone)
+  stream.addBlockOrParagraph(0)
+  while stream.isUnderAnalysis:
+    case stream.char:
+    of '[', ']', '{', '}': stream.updateChildNode(childNode)
+    else: discard
+
+    case stream.char:
+    of '[':
+      childNode = stream.parseLeftBracket()
+    of '{':
+      childNode = stream.parseLeftCurlyBracket()
+    of ']':
+      childNode = stream.parseRightBracket()
+    of '}':
+      childNode = stream.parseRightCurlyBracket()
+    of '\n':
+      if stream.node.children[stream.node.children.high].kind == rapunzelBlock:
+        stream.next()
         continue # ブロック内の場合、改行を無視
       if (childNode.kind == rapunzelText) and (childNode.value != ""):
-        result = result.updateChildNode(childNode, openingParenthesisCount)
+        stream.updateChildNode(childNode)
         childNode = RapunzelNode(kind: rapunzelNone)
-      if rawRapunzel.high >= index + 2:
-        if rawRapunzel[index+1] == '{':
-          result.children.add RapunzelNode(kind: rapunzelBlock)
-        else:
-          result.children.add RapunzelNode(kind: rapunzelParagraph)
+      if stream.str.high >= stream.index + 2:
+        stream.addBlockOrParagraph(+2)
+    of '\t': discard
+
     else:
-      if rawRapunzelChar == '\t':
-        continue
       if childNode.kind == rapunzelNone:
         childNode = RapunzelNode(kind: rapunzelText)
-      childNode.value.add rawRapunzelChar
+      childNode.value.add stream.char
+    
+    stream.next()
+
   if (childNode.kind == rapunzelText) and (childNode.value != ""):
-    result.children[result.children.high].children.add childNode
+    stream.node.children[stream.node.children.high].children.add childNode
+  
+  result = stream.node
